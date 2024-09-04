bac8004441b037e7a3ad2658e0039ef72f45b8ff
Szlachta <kszlacht@stjude.org>
2024-09-04 17:38:13 -0500

    Fix

--- SJ_Haar_CNV/base_search.py
+++ SJ_Haar_CNV/base_search.py
@@ -14,32 +14,24 @@ def haar_low(s,b,e):
     """
     return - np.sqrt(1/(e - b) - 1/(e - s + 1))
 
-def mother_haar(s,b,e,l):
-    """
-    Combine the terms to get the mother wavelet function.
-    """
-    term1 = haar_high(s,b,e) if s <= l <= b else 0
-    term2 = haar_low(s,b,e) if b+1 <= l <= e else 0
-    return term1 + term2
-
 def basis_vector(s,b,e,l):
     """
     This creates a single basis vector for the haar wavelet, paramaterized by s,b,e and l.
     """
     high = haar_high(s,b,e)
     low = haar_low(s,b,e)
-    array = np.zeros(l)
-    array[b:e] = low
-    array[s:b] = high
+    array = np.zeros(e-s)
+    array[0:b-s] = high
+    array[b-s:] = low #should be b+1?
     return array
 
 def haar_matrix(s,e,l):
     """
     This creates the matrix of basis vectors caused by iterating all possible break points.
     """
-    matrix = np.zeros((e-s-1,l))
+    matrix = np.zeros((e-s-1,e-s))
     for i, b in enumerate(range(s+1,e)):
-        matrix[i] = basis_vector(s,b,e,l)
+        matrix[i,:] = basis_vector(s,b,e,l)
     return matrix
 
 def choose_break(signal,s,e):
@@ -49,6 +41,6 @@ def choose_break(signal,s,e):
     """
     l = signal.size
     matrix = haar_matrix(s,e,l)
-    scores = np.matmul(matrix,signal)
-    best_options = np.argwhere(scores == np.nanmax(scores)).flatten() + s + 1
+    scores = np.abs(np.matmul(matrix,signal[s:e]))
+    best_options = np.argwhere(scores == np.nanmax(scores)).flatten() + 1
     return best_options[np.abs(best_options - l //2).argmin()]
