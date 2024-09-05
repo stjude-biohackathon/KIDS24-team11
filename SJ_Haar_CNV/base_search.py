3366ba0025d4ca02a77cadf4d3ca7d3fe5ab4bd9
Tylman-M <tmichael@stjude.org>
2024-09-05 13:38:40 +0000

    Removing dependence on l since we no longer need it

--- SJ_Haar_CNV/base_search.py
+++ SJ_Haar_CNV/base_search.py
@@ -14,7 +14,7 @@ def haar_low(s,b,e):
     """
     return - np.sqrt(1/(e - b) - 1/(e - s + 1))
 
-def basis_vector(s,b,e,l):
+def basis_vector(s,b,e):
     """
     This creates a single basis vector for the haar wavelet, paramaterized by s,b,e and l.
     """
@@ -25,13 +25,13 @@ def basis_vector(s,b,e,l):
     array[b-s:] = low #should be b+1?
     return array
 
-def haar_matrix(s,e,l):
+def haar_matrix(s,e):
     """
     This creates the matrix of basis vectors caused by iterating all possible break points.
     """
     matrix = np.zeros((e-s-1,e-s))
     for i, b in enumerate(range(s+1,e)):
-        matrix[i,:] = basis_vector(s,b,e,l)
+        matrix[i,:] = basis_vector(s,b,e)
     return matrix
 
 def choose_break(signal,s,e):
@@ -39,8 +39,7 @@ def choose_break(signal,s,e):
     This function chooses the best break point for the signal between s and e.
     It resolves ties by selecting the break point closest to the center of the signal.
     """
-    l = signal.size
-    matrix = haar_matrix(s,e,l)
+    matrix = haar_matrix(s,e)
     scores = np.abs(np.matmul(matrix,signal[s:e]))
     best_options = np.argwhere(scores == np.nanmax(scores)).flatten() + 1
-    return best_options[np.abs(best_options - l //2).argmin()]
+    return best_options[np.abs(best_options - signal.size //2).argmin()]
